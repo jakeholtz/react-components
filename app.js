f874148f9ea99d1d810f32ef10dba7233539ed1c
Hack Reactor Student <student@hackreactor.com>
2017-04-12 17:33:53 -0700

    Mostly completes app.js

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
-// TODO
+
+
+
+class GroceryListItem extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      done: false
+    };
+    this.hover = {
+      over: false
+    };
+  }
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  onListItemMouseOver() {
+    this.hover.over = true
+  }
+
+  onListItemMouseOut() {
+    this.hover.over = false
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none',
+      fontWeight: this.hover.over ? 'bold' : 'normal'
+    };
+
+
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)} onMouseOver={this.onListItemMouseOver.bind(this)} onMouseOut={this.onListItemMouseOut.bind(this)}>{this.props.grocery}</li>
+    )
+  }
+}
+
+
+
+var GroceryList = (props) => (
+  <div>
+    <h2>My Grocery List</h2>
+    <ul>
+      {props.groceries.map(grocery =>
+      <GroceryListItem grocery={grocery} />
+      )}
+    </ul>
+  </div>
+)
+
+
+ReactDOM.render(<GroceryList groceries={['cheese', 'pizza']} />, document.getElementById('app'));
